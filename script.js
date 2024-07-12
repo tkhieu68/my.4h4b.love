cfbd5f3761991c755bd5254fed9c92f0d9b3c2df
tkhieu68 <tkhieu68@gmail.com>
2024-07-12 11:13:22 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 document.getElementById('create-vps-btn').addEventListener('click', function() {
-    document.getElementById('popup').style.display = 'flex';
-});
-
-document.getElementById('close-popup-btn').addEventListener('click', function() {
-    document.getElementById('popup').style.display = 'none';
-    window.location.href = 'create-vps.html';
+    setTimeout(function() {
+        alert('VPS created successfully!');
+    }, 4000);
 });
